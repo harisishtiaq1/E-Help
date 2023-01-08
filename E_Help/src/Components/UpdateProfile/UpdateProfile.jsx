3a91c0aa1cdf4605e13bf54f0d88c93a92bd6928
harisishtiaq1 <harisharry232@gmail.com>
2023-01-08 16:39:13 +0500

    Profile

--- E_Help/src/Components/UpdateProfile/UpdateProfile.jsx
+++ E_Help/src/Components/UpdateProfile/UpdateProfile.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function UpdateProfile() {
   return (
     <div className='container'>
-        Update Profile
+        <div className='d-flex align-items-center justify-content-center'>
+            <h1>Profile Update</h1>
+        </div>
+        
     </div>
   )
 }
