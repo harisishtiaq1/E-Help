1a57745d4e3d5cdee5ce0532c38e4a37988c5e2d
Muzammili786 <awanmiqbal2016@gmail.com>
2023-01-11 11:17:33 +0500

    changes made

--- E_Help/src/App.js
+++ E_Help/src/App.js
@@ -7,29 +7,40 @@ import Forgot from "./Components/Forgot/Forgot";
 import Reset from "./Components/ResetPassword/Reset"
 import ContactUS from "./Components/ContactUS/ContactUs"
 import Profile from "./Components/Profile/Profile";
-import Question from "./Components/Question/Question"
+// import Question from "./Components/Question/Question"
 import Answer from "./Components/Answer/Answer";
 import 'react-toastify/dist/ReactToastify.css';
 import { Toaster } from "react-hot-toast";
 import UpdateProfile from "./Components/UpdateProfile/UpdateProfile";
+import Header from "./Components/Header/Header";
+import Question from "./Components/Add-Question/Question";
+import ViewQuestion from "./Components/ViewQuestion";
+import Stackoverflow from "./Components/Stackoverflow";
 function App() {
   return (
-  // <>
-  //   <Toaster/>
-  //     <Routes>
-  //       <Route path="/" exact={true} element={<Login/>} />
-  //       <Route path="/profile" exact={true} element={<Profile/>}/>
-  //       <Route path="/Signup" exact={true} element={<Signup/>} />
-  //       <Route path="/Forgot" exact={true} element={<Forgot/>} />
-  //       <Route path="/" exact={true} element={<Forgot/>} />
-  //       <Route path="/reset" exact={true} element={<Reset/>}/>
-  //     </Routes>
-  //   </>
+  <>
+    <Toaster/>
+    <Header/>
+  
+      <Routes>
+        <Route path="/" exact={true} element={<Login/>} />
+        <Route path="/profile" exact={true} element={<Profile/>}/>
+        <Route path="/Signup" exact={true} element={<Signup/>} />
+        <Route path="/Forgot" exact={true} element={<Forgot/>} />
+        <Route path="/" exact={true} element={<Forgot/>} />
+        <Route path="/reset" exact={true} element={<Reset/>}/>
+        <Route path="/add-question" exact={true} element={<Question/>}/>
+        <Route path="/question" exact={true} element={<ViewQuestion/>}/>
+        <Route path="/Stackoverflow" exact={true} element={<Stackoverflow/>}/>
+        {/* <Route path="/Header" exact={true} element={<Header/>}/> */}
+      </Routes>
+    </>
     // <Profile/>
     // <Question/>
-    <Login/>
+    // <Login/>
     // <ContactUS/>
     // <UpdateProfile/>
+    // <Header />
   );
 }
 export default App;
